45d8704a6d18849f030cb5bc1d2098dc0694f719
OsvaldoPerezOchoa <l19141186@queretaro.tecnm.mx>
2025-08-11 22:06:33 -0600

    darkmode

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,42 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     eventListener();
+
+    darkmode();
 });
 
-function eventListener(){
+function darkmode() {
+
+    const prefDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+
+    if (prefDarkMode.matches) {
+        document.body.classList.add('activado');
+    } else {
+        document.body.classList.remove('activado');
+    }
+
+    prefDarkMode.addEventListener('change', function () {
+        if (prefDarkMode.matches) {
+            document.body.classList.add('activado');
+        } else {
+            document.body.classList.remove('activado');
+        }
+    })
+
+    const darkmode = document.querySelector('.dark-mode');
+
+    darkmode.addEventListener('click', function () {
+        document.body.classList.toggle('activado')
+    })
+}
+
+function eventListener() {
     const mobil = document.querySelector('.mobil-menu');
 
     mobil.addEventListener('click', navegacionResponsive);
 
 }
 
-function navegacionResponsive(){
+function navegacionResponsive() {
     const navegacion = document.querySelector('.nav');
 
     navegacion.classList.toggle('mostrar')
